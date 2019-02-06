1ff1fa1b2ffb70285664d681a7bff8b410e08ec8
cooper09 <cooperatkmba@gmail.com>
2019-02-06 17:59:54 -0500

    resonably clean master version

---
+++ src/containers/GenericClass.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GenericClass(props) {
+
+    return (
+        <h1> Generic Class </h1>
+    )
+}
+GenericClass.propTypes = {
+};
+export default GenericClass;c;
